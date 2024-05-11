9a0f6f17a81dc2ed0b2483ffece7d1b5fe61102e
Tameka Lougoon <mekalougoon@gmail.com>
2024-05-11 16:10:30 +1000

    Nav
    Added functionality for smooth scrolling and some other details, including nav

---
+++ scripts.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+  //  Typing Text Animation  //
+
+  var typed = new Typed(".typing", {
+    strings: [
+      "Software Developer",
+      "Designer",
+      "Games Developer",
+      "Front-end Developer",
+      "Fullstack Developer"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "Software Developer",
+      "Designer",
+      "Games Developer",
+      "Front-end Developer",
+      "Fullstack Developer"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+});
